90e26be55bffa76f20622c40be98fb7aad5e8d1b
telva0527 <telva0527@gmail.com>
2021-04-20 02:25:41 +0900

    chapter02の練習問題を問いた

---
+++ 03_Basics_of_Ruby/chapter02/gets.rb
@@ -0,0 +1,7 @@
+# キーボードから値を入力する
+x = gets
+y = gets
+puts x + y
+x = gets
+y = gets
+puts x.to_i + y.to_i
---
+++ 03_Basics_of_Ruby/chapter02/practice02.rb
@@ -0,0 +1,39 @@
+# 2-1
+# 問１
+puts 2 + 3
+# 問２
+r = 2
+puts r * r * 3.14
+
+# 2-2
+# 問３
+puts "Ruby"
+# 問４
+puts "abc" + "def"
+# 問５
+puts "123".to_i + "456".to_i
+
+# 2-3
+# 問６
+coffee = 300
+esplessoshot = 100
+total = coffee + espresso * 2
+puts "コーヒー:#{coffee}円"
+puts "合計:#{total}円"
+
+# 問７
+coffee = 400
+esplessoshot = 100
+total = coffee + espresso * 2
+puts "コーヒー:#{coffee}円"
+puts "合計:#{total}円"
+
+# 問８
+total = 300 * 2
+p total
+tax = total * 0.08
+puts tax
+
+# 問９
+total = 300 * 2
+puts total
